a9fa89118589a7d2f84fc13cd489a01334d1a9a2
knbaskar <bhavin2010dec@gmail.com>
2024-12-26 10:31:01 +0530

    meta tag update in app.js
    meta tag update in app.js

--- pages/_app.js
+++ pages/_app.js
@@ -77,13 +77,62 @@ export default class MyApp extends App {
   }
   render() {
     const { Component, pageProps } = this.props;
+    const metadata = {
+      title: {
+        template: '%s | Dave Gray',
+        default: 'Dave Gray Teaches Code',
+      },
+      description: "Hello, I'm Dave. 👋 I teach coding and web development skills.",
+      applicationName: 'Book My Service',
+      authors: [{ name: 'BMS' }],
+      generator: 'Next.js',
+      keywords: ['bookmyservice', 'bms', 'code', 'web development', 'javascript', 'react', 'node.js', 'next.js', 'web dev', 'html', 'css', 'python'],
+      referrer: 'origin-when-cross-origin',
+      creator: 'BMS Team',
+      publisher: 'BMS Team',
+      verification: {
+        google: 'A3y6FQxQnzZJy3m7zE8EHOiX9Fy34uQzSDHG_0muZhw',
+      },
+    };
     return (
       <React.Fragment>
         <Head>
           <meta
             name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+            content="width=device-width, i
+            nitial-scale=1, shrink-to-fit=no hjhjhjhj"
           />
+          <title>{metadata.title.default}</title>
+
+          {/* Description */}
+          <meta name="description" content={metadata.description} />
+
+          {/* Application Name */}
+          <meta name="application-name" content={metadata.applicationName} />
+
+          {/* Authors */}
+          {metadata.authors.map((author, index) => (
+            <meta key={index} name="author" content={author.name} />
+          ))}
+
+          {/* Generator */}
+          <meta name="generator" content={metadata.generator} />
+
+          {/* Keywords */}
+          <meta name="keywords" content={metadata.keywords.join(', ')} />
+
+          {/* Referrer Policy */}
+          <meta name="referrer" content={metadata.referrer} />
+
+          {/* Creator */}
+          <meta name="creator" content={metadata.creator} />
+
+          {/* Publisher */}
+          <meta name="publisher" content={metadata.publisher} />
+
+          {/* Google Site Verification */}
+          <meta name="google-site-verification" content={metadata.verification.google} />
+
           {/* <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE" /> */}
           <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap" async defer></script>
           <title>{BOOK_MY_SERVICE}</title>
