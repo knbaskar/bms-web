f66bf8ee4d7de677384fcd7bbc75ccba4457b657
knbaskar <bhavin2010dec@gmail.com>
2025-01-05 16:31:41 +0530

    about us updates

--- pages-sections/about-us/SectionDescription.js
+++ pages-sections/about-us/SectionDescription.js
@@ -20,13 +20,7 @@ export default function SectionDescription() {
           sm={8}
           className={classNames(classes.mrAuto, classes.mlAuto)}
         >
-          <h5 className={classes.description}>
-            This is the paragraph where you can write more details about your
-            product. Keep you user engaged by providing meaningful information.
-            Remember that by this time, the user is curious, otherwise he wouldn
-            {"'"}t scroll to get here. Add a button if you want the user to see
-            more.
-          </h5>
+          
         </GridItem>
       </GridContainer>
     </div>
